a7c3860ef80323e5d9134d86f1d9ee8a6a338d76
Oliver Findl <github@findl.sk>
2020-10-16 19:40:36 +0200

    Updated dependencies

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,17 +3,12 @@
 module.exports = {
 	"env": {
 		"commonjs": true,
-		"es6": true,
+		"es2021": true,
 		"node": true
 	},
 	"extends": "eslint:recommended",
-	"globals": {
-		"Atomics": "readonly",
-		"SharedArrayBuffer": "readonly"
-	},
 	"parserOptions": {
-		"ecmaVersion": 2018,
-		"sourceType": "module"
+		"ecmaVersion": 12
 	},
 	"rules": {
 		"indent": [
