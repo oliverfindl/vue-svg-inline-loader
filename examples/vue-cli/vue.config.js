2678387f693b9cfef5c0e872524c5d711910e880
Oliver Findl <github@findl.sk>
2020-12-03 21:47:00 +0100

    Recreated Vue CLI example project

--- examples/vue-cli/vue.config.js
+++ examples/vue-cli/vue.config.js
@@ -1,12 +1,12 @@
-const { join } = require('path');
+const { join } = require('path')
 
 module.exports = {
-    chainWebpack: config => {
-        config.module
-            .rule('vue')
-            .use('vue-svg-inline-loader')
-//              .loader('vue-svg-inline-loader') // in your project
-                .loader(join(__dirname, '../../index.js'))
-                .options({ /* ... */ });
-    }
+  chainWebpack: config => {
+    config.module
+      .rule('vue')
+      .use('vue-svg-inline-loader')
+//      .loader('vue-svg-inline-loader') // in your project
+        .loader(join(__dirname, '../../index.js'))
+        .options({ /* ... */ })
+  }
 }
