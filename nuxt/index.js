9893bb7da600810d17241cf6d47be87e1a7e3553
Sébastien LeBlanc <mrsebastienleblanc@icloud.com>
2022-06-28 15:43:14 -0400

    Nuxt module (#54)
    Nuxt module
    
    Co-authored-by: Sébastien LeBlanc
    Co-authored-by: Oliver Findl

---
+++ nuxt/index.js
@@ -0,0 +1,30 @@
+const { join } = require("path");
+
+export default function vueSvgInlineLoaderModule(moduleOptions) {
+	const options = Object.assign(
+		{},
+		this.options.vueSvgInlineLoader,
+		moduleOptions
+	);
+
+	this.extendBuild((config) => {
+		const vueRule = config.module.rules.find(
+			({ test }) => test.toString() === /\.vue$/i.toString()
+		);
+		if (!vueRule.use) {
+			vueRule.use = [
+				{
+					loader: vueRule.loader,
+					options: vueRule.options,
+				},
+			];
+			delete vueRule.loader;
+			delete vueRule.options;
+		}
+
+		vueRule.use.push({
+			loader: join(__dirname, "../index.js"),
+			options,
+		});
+	});
+}
